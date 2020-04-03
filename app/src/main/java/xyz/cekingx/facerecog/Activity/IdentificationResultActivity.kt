22580d954f9434fdbb5b7224f037d70e78495bb5
Dirga Yasa <cekingx@gmail.com>
2020-04-03 14:58:14 +0800

    IdentificationResutActivity

--- app/src/main/java/xyz/cekingx/facerecog/Activity/IdentificationResultActivity.kt
+++ app/src/main/java/xyz/cekingx/facerecog/Activity/IdentificationResultActivity.kt
@@ -1,8 +1,11 @@
 package xyz.cekingx.facerecog.Activity
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_identification_result.*
 import xyz.cekingx.facerecog.R
 
 class IdentificationResultActivity : AppCompatActivity() {
@@ -10,5 +13,14 @@ class IdentificationResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_identification_result)
+
+        val url = intent.getStringExtra("url")
+        val uri: Uri = Uri.parse(url);
+
+        result.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
     }
 }
